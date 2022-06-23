82b913de420f1a69fcf78858a65084a0545c1150
managment_system <mas152q@gmail.com>
2022-06-23 16:15:01 +1000

    portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Portfolio extends Component {
-  render() {
+const Portfolio = () => {
+  if (!props.data) {
+    return null;
+  }
+  return (
+    <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Work.</h1>
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+          <div
+            id="portfolio-wrapper"
+            className="bgrid-quarters s-bgrid-thirds cf"
+          >
+            {props.data.projects.map(function (projects) {
+              var projectImage = 'images/portfolio/' + projects.image;
+              return (
+                <div key={projects.title} className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href={projects.url} title={projects.title}>
+                      <img alt={projects.title} src={projectImage} />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{projects.title}</h5>
+                          <p>{projects.category}</p>
+                        </div>
+                      </div>
+                      <div className="link-icon">
+                        <i className="fa fa-link"></i>
+                      </div>
+                    </a>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
+              );
+            })}
           </div>
         </div>
-      })
-    }
-
-    return (
-      <section id="portfolio">
-
-      <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Check Out Some of My Work.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
-            </div>
-          </div>
       </div>
-   </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default Portfolio;
